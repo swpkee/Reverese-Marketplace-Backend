f0c5ec965453b6519137abf944641970a48142b8
swpkee <aodzaza1130@gmail.com>
2021-10-01 20:42:57 +0700

    error for aom

--- config/database.js
+++ config/database.js
@@ -2,14 +2,19 @@ module.exports = ({ env }) => ({
   defaultConnection: 'default',
   connections: {
     default: {
-      connector: 'bookshelf',
+      connector: 'mongoose',
       settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+        host: env('DATABASE_Host',"reverse-marketplace.q2k9a.mongodb.net"),
+        srv: env.bool('DATABASE_SRV', true),
+        port: env.int('DATABASE_PORT', 27017),
+        database: env('DATABASE_NAME',"Reverse-marketplace"),
+        username: env('DATABASE_USER',"reversemarketplace1"),
+        pasword: env('DATABASE_PASSWORD',"CUAE1P0Dqiq24J1T")
       },
       options: {
-        useNullAsDefault: true,
+        authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
+        ssl: env.bool('DATABASE_SSL', true),
       },
     },
   },
-});
+})
